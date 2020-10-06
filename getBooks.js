9766097128260f4c98d1990d5b5017fcc49d0a80
HeroicNacho <72362346+HeroicNacho@users.noreply.github.com>
2020-10-05 21:33:14 -0400

    Create getBooks.js

---
+++ getBooks.js
@@ -0,0 +1,5 @@
+alert("We are checking for book matches!!")
+
+var books = [
+  "Python-printing text":"This book teaches how to print text in Python, and other useful resources that can be used to print text."
+]
