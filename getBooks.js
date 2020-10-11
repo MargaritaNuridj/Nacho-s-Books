841a914588240d3905a805c64482526e085ea2c1
MargaritaNuridj <31721016+MargaritaNuridj@users.noreply.github.com>
2020-10-10 20:17:37 -0700

    Add files via upload

--- getBooks.js
+++ getBooks.js
@@ -1,10 +1,10 @@
-windows.onload = function() {
+window.onload = function (){
   alert("We are checking for book matches!")
-}
+};
 
 
 alert("We are checking for book matches!!")
 
-var books = [
+var books = {
   "Python-printing text":"This book teaches how to print text in Python, and other useful resources that can be used to print text."
-]
+};
